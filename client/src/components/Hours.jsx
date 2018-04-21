141a3affa94a7a5334e7602bdfdd76d1202b8e76
Pooja Kodavanti <pkodavan@gmail.com>
2018-04-21 14:04:20 -0600

    Change Hours and Contact to stateful

--- client/src/components/Hours.jsx
+++ client/src/components/Hours.jsx
@@ -4,39 +4,40 @@ import {Icon} from 'react-materialize';
 import moment from 'moment';
 
 
-const Hours = (props) => {
-  $(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
-  console.log(props.hours)
+export default class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currHours: [],
+      toggleHours: false
+    };
 
  
-  // find current day
-  let currDay = moment().weekday(); // Mon = 1, Tues = 2, etc.
-  let currHours = props.hours.slice(currDay-1).concat(props.hours.slice(0, currDay-1));
-  console.log(currHours)
-
-  // check if now closed
+  };
+  sortHours() {
+    let currDay = moment().weekday(); // Mon = 1, Tues = 2, etc.
+    let currHours = props.hours.slice(currDay-1).concat(props.hours.slice(0, currDay-1));
+    console.log(currHours)
+  }
 
   
-  return ( 
-    <div className="hours">
-      <ul className="collapsible">
-        <li>
-          <div className="collapsible-header">{currHours[0]}<Icon></Icon></div>
-          <div className="collapsible-body">
-            {currHours.slice(1).map(day => {
-              <span>{day}</span>
-            })}
-          </div>
-        </li>
-      </ul>
-    </div>
-  )
+
+  render() {
+
+    return (
+      <div className="hours">
+        <ul className="collapsible">
+          <li>
+            <div className="collapsible-header">{currHours[0]}<Icon></Icon></div>
+            <div className="collapsible-body">
+              {currHours.slice(1).map(day => {
+                <span>{day}</span>
+              })}
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
-
-// Hours.propTypes = {
-//   hours: React.PropTypes.array.isRequired
-// };
-
-export default Hours;
