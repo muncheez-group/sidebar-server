fb3964f27b9f6ad4207b8daa03769139dc1283ed
Pooja Kodavanti <pkodavan@gmail.com>
2018-04-24 12:37:01 -0700

    Adjustments to css

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import Hours from './Hours.jsx';
 import Contact from './Contact.jsx';
 import GMap from './GMap.jsx';
 const axios = require('axios');
-// import '../../dist/style.css';
+import '../../dist/style.css';
 
 
 export default class Sidebar extends React.Component {
