ff7252d72d0eb21311715c26c0139713abda7a17
Michael Dimitras <dimitrasprojects@gmail.com>
2018-05-04 16:39:44 -0700

    Data generator now outputs both csv and json

--- data_generator/dataGenerator.js
+++ data_generator/dataGenerator.js
@@ -4,15 +4,28 @@ const fs = require('fs');
 
 faker.locale = 'en_US';
 
-const stream = fs.createWriteStream('./data.json');
+/*
+ *  If the script was run with --csv output data as a csv,
+ *  otherwise data will be output as JSON
+ */
+const args = process.argv.slice(2);
+const outputCSV = args.includes('--csv');
 
-const hours = `Monday: 9:00 AM – 9:00 PM,
-               Tuesday: 9:00 AM – 9:00 PM,
-               Wednesday: 9:00 AM – 9:00 PM,
-               Thursday: 9:00 AM – 9:00 PM,
-               Friday: 9:00 AM – 9:00 PM,
-               Saturday: 9:00 AM – 9:00 PM,
-               Sunday: 9:00 AM – 9:00 PM`;
+/*
+ * If a number was passed in on the command line,
+ * generate that many records, otherwise generate 10
+ */
+const numberToGenerate = args.find(elem => /\b\d+\b/.test(elem)) || 10;
+
+const stream = fs.createWriteStream(`./data.${outputCSV ? 'csv' : 'json'}`);
+
+const hours = 'Monday: 9:00 AM – 9:00 PM, ' +
+              'Tuesday: 9:00 AM – 9:00 PM, ' +
+              'Wednesday: 9:00 AM – 9:00 PM, ' +
+              'Thursday: 9:00 AM – 9:00 PM, ' +
+              'Friday: 9:00 AM – 9:00 PM, ' +
+              'Saturday: 9:00 AM – 9:00 PM, ' +
+              'Sunday: 9:00 AM – 9:00 PM';
 
 function generateName() {
   let adj = dict.adjectives[Math.floor(Math.random() * dict.adjectives.length)];
@@ -24,7 +37,7 @@ function generateName() {
   return `${adj} ${noun}`;
 }
 
-function createRecord(i) {
+function createJSONRecord(i) {
   faker.seed(i);
   const restaurant = {};
   restaurant.id = i; // generate incrementing id
@@ -33,30 +46,43 @@ function createRecord(i) {
   restaurant.url = faker.internet.url();
   restaurant.phone = faker.phone.phoneNumberFormat(0);
   restaurant.hours = hours;
-  restaurant.coords = {
-    lat: faker.address.latitude(),
-    long: faker.address.longitude(),
-  };
+  restaurant.lat = faker.address.latitude();
+  restaurant.long = faker.address.longitude();
   return JSON.stringify(restaurant);
+}
+
+function createCSVRecord(i) {
+  faker.seed(i);
+  let restaurant = '';
+  restaurant += `${i},`;
+  restaurant += `${generateName()},`;
+  restaurant += `${faker.address.streetAddress()},`;
+  restaurant += `${faker.internet.url()},`;
+  restaurant += `${faker.phone.phoneNumberFormat(0)},`;
+  restaurant += `"${hours}",`;
+  restaurant += `${faker.address.latitude()},`;
+  restaurant += `${faker.address.longitude()}\n`;
+  return restaurant;
 }
 
 /*
  * Taken from the Node.js docs
  * https://nodejs.org/api/stream.html#stream_event_drain
  */
-function writer(numberToGenerate = 10000000) {
+function writer() {
   let i = numberToGenerate;
   function write() {
     let ok = true;
+    if (outputCSV) stream.write('id,name,address,url,phone,hours,lat,long\n');
     do {
       i -= 1;
       if (i === 0) {
         // last time!
-        stream.write(createRecord(i));
+        outputCSV ? stream.write(createCSVRecord(i)) : stream.write(createJSONRecord(i));
       } else {
         // see if we should continue, or wait
         // don't pass the callback, because we're not done yet.
-        ok = stream.write(createRecord(i));
+        ok = outputCSV ? stream.write(createCSVRecord(i)) : stream.write(createJSONRecord(i));
       }
     } while (i > 0 && ok);
     if (i > 0) {
@@ -71,6 +97,7 @@ function writer(numberToGenerate = 10000000) {
 writer();
 
 module.exports = {
-  createRecord,
+  createCSVRecord,
+  createJSONRecord,
   generateName,
 };
